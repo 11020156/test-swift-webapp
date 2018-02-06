84cb4051ec97832abce1374f9f6c1a7219702cf6
terrence <11020156@life.hkbu.edu.hk>
2018-02-06 11:30:24 +0800

    test commit

--- Package.swift
+++ Package.swift
@@ -1,25 +1,19 @@
-// swift-tools-version:4.0
-
 import PackageDescription
 
 let package = Package(
-    name: "test-swift-webapp",
-    products: [
-        .library(name: "App", targets: ["App"]),
-        .executable(name: "Run", targets: ["Run"])
-    ],
+    name: "hello-persistence",
     dependencies: [
-        .package(url: "https://github.com/vapor/vapor.git", .upToNextMajor(from: "2.1.0")),
-        .package(url: "https://github.com/vapor/fluent-provider.git", .upToNextMajor(from: "1.2.0")),
+        .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1, minor: 1),
+        .Package(url: "https://github.com/vapor/postgresql-provider",
+            majorVersion: 1, minor: 0)
     ],
-    targets: [
-        .target(
-            name: "App",
-            dependencies: ["Vapor", "FluentProvider"],
-            exclude: ["Config", "Public", "Resources"]
-        ),
-        .target(name: "Run", dependencies: ["App"]),
-        .testTarget(name: "AppTests", dependencies: ["App", "Testing"])
+    exclude: [
+        "Config",
+        "Database",
+        "Localization",
+        "Public",
+        "Resources",
+        "Tests",
     ]
 )
 
